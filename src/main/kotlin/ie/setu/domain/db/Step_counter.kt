cfb11a88b0d55fac6a904706e527500f0bc8c0ce
Ammad Sarwar <20100780@mail.wit.ie>
2022-12-29 20:48:56 +0000

    ...

---
+++ src/main/kotlin/ie/setu/domain/db/Step_counter.kt
@@ -0,0 +1,14 @@
+package ie.setu.domain.db
+
+import ie.setu.domain.db.Health_coaching.autoIncrement
+import ie.setu.domain.db.Health_coaching.primaryKey
+import ie.setu.domain.db.Health_coaching.references
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+
+object Step_counter: Table("CoachingActivity") {
+
+    val id = integer("ID").autoIncrement().primaryKey()
+    val steps = integer("Daily_Steps")
+    val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
+}
