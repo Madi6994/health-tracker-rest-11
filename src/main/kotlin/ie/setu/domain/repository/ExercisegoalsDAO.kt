ee81dc47e315b425b7b679719f6331b74baadc14
Ammad Sarwar <20100780@mail.wit.ie>
2022-12-29 22:00:40 +0000

    ...

--- src/main/kotlin/ie/setu/domain/repository/ExercisegoalsDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/ExercisegoalsDAO.kt
@@ -1,4 +1,22 @@
 package ie.setu.domain.repository
 
+import ie.setu.domain.Exercise_goals
+import ie.setu.domain.Step_Counter
+import ie.setu.domain.db.Exercise_goal
+import ie.setu.domain.db.Step_counter
+import ie.setu.utils.mapToExercise_goal
+import ie.setu.utils.mapToStep_counter
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
 class ExercisegoalsDAO {
+
+    fun getAll(): ArrayList<Exercise_goals> {
+        val exercisegoalsList: ArrayList<Exercise_goals> = arrayListOf()
+        transaction {
+            Exercise_goal.selectAll().map {
+                exercisegoalsList.add(mapToExercise_goal(it)) }
+        }
+        return exercisegoalsList
+    }
 }
