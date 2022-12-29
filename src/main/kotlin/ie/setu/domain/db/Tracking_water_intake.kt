4f81b6443764b1064a8942903d0a6347e9a21d99
Ammad Sarwar <20100780@mail.wit.ie>
2022-12-29 20:53:03 +0000

    ...

---
+++ src/main/kotlin/ie/setu/domain/db/Tracking_water_intake.kt
@@ -0,0 +1,26 @@
+package ie.setu.domain.db
+
+import ie.setu.domain.db.Health_coaching.autoIncrement
+import ie.setu.domain.db.Health_coaching.primaryKey
+import ie.setu.domain.db.Health_coaching.references
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+
+object Tracking_water_intake : Table("WaterIntakeActivity") {
+    val id = integer("ID").autoIncrement().primaryKey()
+    val glass_of_water = integer("Glass_of_Water")
+    val datetime = integer("DateTime")
+    val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
+}
+
+
+
+
+
+
+
+
+
+
+
+
