602340a1226ccacc77bd1173164616bb2fdc67a8
Ammad Sarwar <20100780@mail.wit.ie>
2022-12-30 00:30:59 +0000

    ...

--- src/test/kotlin/ie/setu/repository/ExercisegoalsDAOTest.kt
+++ src/test/kotlin/ie/setu/repository/ExercisegoalsDAOTest.kt
@@ -1,5 +1,7 @@
 package ie.setu.repository
 
+import ie.setu.domain.Activity
+import ie.setu.domain.Exercise_goals
 import ie.setu.domain.db.Activities
 import ie.setu.domain.db.Exercise_goal
 import ie.setu.domain.repository.ActivityDAO
@@ -8,6 +10,7 @@ import ie.setu.helpers.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -138,4 +141,105 @@ class ExercisegoalsDAOTest {
             }
         }
     }
+
+    @Nested
+    inner class UpdateActivities {
+        @Test
+        fun `updating existing activity in table results in successful update`() {
+            transaction {
+
+                //Arrange - create and populate tables with three users and three activities
+                val userDAO = populateUserTable()
+                val CaloriesDAO = populateExercise_goalsTable()
+
+                //Act & Assert
+                val Caloriesupdate = Exercise_goals(id = 1, Calories_To_Burn = 155, Steps = 65, Date = DateTime.now(), userId = 1)
+                CaloriesDAO.updateByexerciseId(Caloriesupdate.id, Caloriesupdate)
+                assertEquals(Caloriesupdate, CaloriesDAO.findByexerciseId(3))
+            }
+        }
+
+        @Test
+        fun `updating non-existant activity in table results in no updates`() {
+            transaction {
+
+                //Arrange - create and populate tables with three users and three activities
+                val userDAO = populateUserTable()
+                val CaloriesDAO = populateExercise_goalsTable()
+
+                //Act & Assert
+                val Caloriesupdate = Exercise_goals(id = 1, Calories_To_Burn = 155, Steps = 65, Date = DateTime.now(), userId = 1)
+                CaloriesDAO.updateByexerciseId(4, Caloriesupdate)
+                assertEquals(null, CaloriesDAO.findByexerciseId(4))
+                assertEquals(5, CaloriesDAO.getAll().size)
+            }
+        }
+    }
+
+    @Nested
+    inner class DeleteActivities {
+        @Test
+        fun `deleting a non-existant activity (by id) in table results in no deletion`() {
+            transaction {
+
+                //Arrange - create and populate tables with three users and three activities
+                val userDAO = populateUserTable()
+                val CaloriesDAO = populateExercise_goalsTable()
+
+                //Act & Assert
+                assertEquals(5, CaloriesDAO.getAll().size)
+                CaloriesDAO.deleteByexerciseId(4)
+                assertEquals(5, CaloriesDAO.getAll().size)
+            }
+        }
+
+        @Test
+        fun `deleting an existing activity (by id) in table results in record being deleted`() {
+            transaction {
+
+                //Arrange - create and populate tables with three users and three activities
+                val userDAO = populateUserTable()
+                val CaloriesDAO = populateExercise_goalsTable()
+
+                //Act & Assert
+                assertEquals(5, CaloriesDAO.getAll().size)
+                CaloriesDAO.deleteByexerciseId(activity3.id)
+                assertEquals(3, CaloriesDAO.getAll().size)
+            }
+        }
+
+        @Test
+        fun `deleting activities when none exist for user id results in no deletion`() {
+            transaction {
+
+                //Arrange - create and populate tables with three users and three activities
+                val userDAO = populateUserTable()
+                val CaloriesDAO = populateExercise_goalsTable()
+
+                //Act & Assert
+                assertEquals(3, CaloriesDAO.getAll().size)
+                CaloriesDAO.deleteByUserId(3)
+                assertEquals(3, CaloriesDAO.getAll().size)
+            }
+        }
+
+
+
+        @Test
+        fun `deleting activities when 1 or more exist for user id results in deletion`() {
+            transaction {
+
+                //Arrange - create and populate tables with three users and three activities
+                val userDAO = populateUserTable()
+                val CaloriesDAO = populateExercise_goalsTable()
+
+                //Act & Assert
+                assertEquals(3, CaloriesDAO.getAll().size)
+                CaloriesDAO.deleteByUserId(1)
+                assertEquals(1, CaloriesDAO.getAll().size)
+            }
+        }
+    }
+
+
 }
